e68609bb66efcd01ed83eb3e7a60498e652edad1
Rangga Adhitya <deneuv3.4@gmail.com>
2017-04-08 12:03:28 +0700

    add style

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -28,21 +28,24 @@ class App extends Component {
       text: ''
     }));
   }
-  
 
   render() {
     return (
-      <div>
-        <h3>TODO APP</h3>
-        <ul>
-          {this.state.items.map(item => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-        </ul>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
+      <div className='App'>
+        <h3 className='App-header'>TODO APP</h3>
+        <div className='App-intro'>
+          <ul>
+            {this.state.items.map(item => (
+              <li key={item.id}>{item.text}</li>
+            ))}
+          </ul>
+        </div>
+        <div className='App-intro'>
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChange} value={this.state.text} />
+            <button>{'New Todo #' + (this.state.items.length + 1)}</button>
+          </form>
+        </div>
       </div>
     );
   }
